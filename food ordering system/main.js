5e57c2f39dbc6ee24b08aee78023a6f368a9fa98
Aftab <98654098+aftxb@users.noreply.github.com>
2022-07-16 19:25:02 +0530

    Add files via upload

---
+++ food ordering system/main.js
@@ -0,0 +1,30 @@
+function button() {
+  document.getElementById("nav").classList.toggle("active");
+  document.getElementById("hero").click("active");
+  document.getElementById("modal").click("active")
+}
+
+
+
+
+
+
+
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2500);
+}
